a32e85e890531524944eebb44554dc64313de70f
Mahenina <mahenina@gmail.com>
2015-07-12 22:32:25 +0300

    submitting graph 2

---
+++ plot2.R
@@ -0,0 +1,14 @@
+library("sqldf") # In order to use sql to filter the data to import
+
+f<-file("household_power_consumption.txt")
+
+cons <- sqldf("select * from f where Date in ('1/2/2007','2/2/2007')",
+              dbname = tempfile(), file.format = list(header = T, row.names = F, sep=";"))
+
+cons$Time<-strptime(paste(cons$Date,cons$Time),"%d/%m/%Y %H:%M:%S")
+cons$Date<-as.Date(cons$Date, "%d/%m/%Y")
+
+rm(f)
+opar=par(ps=12)
+plot(cons$Time,cons$Global_active_power, type="l",xlab="", ylab="Global Active Power (kilowatts)")
+opar
